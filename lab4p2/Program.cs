ba41adaed8d6435a349ed2bf1533d51130fe2bb6
ScottWeber27 <webers04@uwosh.edu>
2021-10-05 12:31:25 -0500

    Added IsDiagonal stub and comment

--- lab4p2/Program.cs
+++ lab4p2/Program.cs
@@ -6,7 +6,17 @@ namespace lab4p2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="arr">a 2d integer array</param>
+        /// <returns>Returns true if and only if the array is a square size, and contains only diagonal elements.</returns>
+        public static bool IsDiagonal(int[,] arr)
+        {
+            throw new NotImplementedException();
         }
     }
 }
