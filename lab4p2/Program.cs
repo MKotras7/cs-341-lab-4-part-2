4a4b85e10db6adb66220459baa3795d3986361ff
ScottWeber27 <webers04@uwosh.edu>
2021-10-11 15:47:34 -0500

    Scott adding comment.

--- lab4p2/Program.cs
+++ lab4p2/Program.cs
@@ -10,7 +10,7 @@ namespace lab4p2
         }
 
         /// <summary>
-        /// 
+        /// This is Scott's Comment 
         /// </summary>
         /// <param name="arr">a 2d integer array</param>
         /// <returns>Returns true if and only if the array is a square size, and contains only diagonal elements.</returns>
